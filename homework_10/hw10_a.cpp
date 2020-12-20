19672fa6844b8d9846722097e784b5f548a5c88a
Lazar M. Cvetković <l.cvetkovic.997@gmail.com>
2020-12-20 17:51:46 +0100

    Homework 10 - final version

--- homework_10/hw10_a.cpp
+++ homework_10/hw10_a.cpp
@@ -1,11 +1,12 @@
 //
-// Created by jugos000 on 18-Dec-20.
+// Created by jugos000 on 20-Dec-20.
 //
 
 #include <cmath>
 #include <iomanip>
 #include <iostream>
 #include <random>
+#include <unordered_map>
 
 using namespace std;
 
@@ -15,151 +16,151 @@ constexpr double c2 = 1.494;
 constexpr double v_max = 0.2;
 constexpr double dt = 1;
 
-constexpr long NUMBER_OF_ITERATIONS = 100000;
+constexpr long NUMBER_OF_ITERATIONS = 50;
 constexpr long SWARM_DIMENSION = 2000;
 
 random_device rd;
 mt19937 mt(rd());
 
-struct point {
+struct Point {
     double x;
     double y;
     double z;
 
-    point(double x, double y, double z) : x(x), y(y), z(z) {}
+    Point() {}
 
-    point operator+(const point &p) const {
-        return point(p.x + x, p.y + y, p.z + z);
+    constexpr Point(double x, double y, double z) : x(x), y(y), z(z) {}
+
+    friend ostream &operator<<(ostream &os, const Point &p) {
+        os << "(" << fixed << setprecision(3) << p.x << ", " << fixed << setprecision(3) << p.y << ", " << fixed
+           << setprecision(3) << p.z << ")";
+        return os;
     }
 
-    point operator*(double w) const {
-        return point(w * x, w * y, w * z);
+    friend Point operator+(const Point &p1, const Point &p2) {
+        return Point(p1.x + p2.x, p1.y + p2.y, p1.z + p2.z);
     }
 
-    friend ostream &operator<<(ostream &os, const point &p) {
-        os << "(" << fixed << setprecision(2) << p.x << ", " << fixed << setprecision(2) << p.y << ", " << fixed
-           << setprecision(2) << p.z << ")";
+    friend Point operator-(const Point &p1, const Point &p2) {
+        return Point(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z);
+    }
+
+    friend Point operator*(const double c, const Point &p) {
+        return Point(c * p.x, c * p.y, c * p.z);
+    }
+
+    static double euclideanDistance(const Point &p1, const Point &p2) {
+        return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2) + pow(p1.z - p2.z, 2));
+    }
+
+    static Point generateRandomPoint() {
+        static uniform_real_distribution<double> randomGeneratorXY(0, 10);
+        static uniform_real_distribution<double> randomGeneratorZ(0, 4);
+
+        return Point(randomGeneratorXY(mt), randomGeneratorXY(mt), randomGeneratorZ(mt));
+    }
+
+    static Point generateRandomSpeed() {
+        //static uniform_real_distribution<double> randomGenerator(0, 0.01);
+
+        return Point(0.01, 0.01, 0.01);
+    }
+};
+
+struct Vector;
+
+inline double costFunction(const Vector &v);
+
+struct Vector {
+    Point S1;
+    Point S2;
+
+    Vector() {}
+
+    Vector(const Point &s1, const Point &s2) : S1(s1), S2(s2) {}
+
+    friend Vector operator+(const Vector &v1, const Vector &v2) {
+        return Vector(v1.S1 + v2.S1, v1.S2 + v2.S2);
+    }
+
+    friend Vector operator-(const Vector &v1, const Vector &v2) {
+        return Vector(v1.S1 - v2.S1, v1.S2 - v2.S2);
+    }
+
+    friend Vector operator*(const double c, const Vector &v) {
+        return Vector(c * v.S1, c * v.S2);
+    }
+
+    friend bool operator<(const Vector &v1, const Vector &v2) {
+        double f1 = costFunction(v1);
+        double f2 = costFunction(v2);
+
+        return f1 < f2;
+    }
+
+    friend ostream &operator<<(ostream &os, const Vector &v) {
+        os << "(S1, S2, cost) = (" << v.S1 << ", " << v.S2 << ", " << fixed << setprecision(3) << costFunction(v)
+           << ")";
         return os;
     }
 };
 
-const point A(1, 5, 1);
-const point B(3, 2, 0);
-const point C(5, 6, 1);
-const point D(6, 3, 3);
+constexpr Point A(1.0, 5.0, 1.0);
+constexpr Point B(3.0, 2.0, 0.0);
+constexpr Point C(5.0, 6.0, 1.0);
+constexpr Point D(6.0, 3.0, 3.0);
 
-struct solution {
-    point S1;
-    point S2;
-
-    point bestS1;
-    point bestS2;
-    double bestScore = numeric_limits<double>::max();
-
-    point v1;
-    point v2;
-
-    solution(const point &s1, const point &s2, const point &v1, const point &v2) :
-            S1(s1),
-            S2(s2),
-            v1(v1),
-            v2(v2),
-            bestS1(s1),
-            bestS2(s2) {}
-
-    void updateSpeed(const solution &g) {
-        static uniform_real_distribution<double> r(0, 1);
-
-        v1.x = w * v1.x + c1 * r(mt) * (bestS1.x - S1.x) + c2 * r(mt) * (g.S1.x - S1.x);
-        v1.y = w * v1.y + c1 * r(mt) * (bestS1.y - S1.y) + c2 * r(mt) * (g.S1.y - S1.y);
-        v1.z = w * v1.z + c1 * r(mt) * (bestS1.z - S1.z) + c2 * r(mt) * (g.S1.z - S1.z);
-
-        v2.x = w * v2.x + c1 * r(mt) * (bestS2.x - S2.x) + c2 * r(mt) * (g.S2.x - S2.x);
-        v2.y = w * v2.y + c1 * r(mt) * (bestS2.y - S2.y) + c2 * r(mt) * (g.S2.y - S2.y);
-        v2.z = w * v2.z + c1 * r(mt) * (bestS2.z - S2.z) + c2 * r(mt) * (g.S2.z - S2.z);
-    }
-
-    void updatePosition() {
-        S1.x = S1.x + dt * v1.x;
-        S1.y = S1.y + dt * v1.y;
-        S1.z = S1.z + dt * v1.z;
-
-        S2.x = S2.x + dt * v2.x;
-        S2.y = S2.y + dt * v2.y;
-        S2.z = S2.z + dt * v2.z;
-    }
-};
-
-inline double euclideanDistance(const point &p1, const point &p2) {
-    return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2) + pow(p1.z - p2.z, 2));
-}
-
-inline double optimizationFunction(const solution &s) {
-    double a_s1 = euclideanDistance(A, s.S1);
-    double b_s1 = euclideanDistance(B, s.S1);
-    double s1_s2 = euclideanDistance(s.S1, s.S2);
-    double c_s2 = euclideanDistance(C, s.S2);
-    double d_s2 = euclideanDistance(D, s.S2);
+inline double costFunction(const Vector &v) {
+    double a_s1 = Point::euclideanDistance(A, v.S1);
+    double b_s1 = Point::euclideanDistance(B, v.S1);
+    double s1_s2 = Point::euclideanDistance(v.S1, v.S2);
+    double c_s2 = Point::euclideanDistance(C, v.S2);
+    double d_s2 = Point::euclideanDistance(D, v.S2);
 
     return a_s1 + b_s1 + s1_s2 + c_s2 + d_s2;
 }
 
 int main() {
-    // AGENT -> EQ. individual
-    // SWARM -> EQ. population
-    solution g(point(1, 1, 1),
-               point(1, 1, 1),
-               point(0.01, 0.01, 0.01),
-               point(0.01, 0.01, 0.01));
-    long iteration = 0;
+    Vector bestSolution;
 
-    auto **swarm = new solution *[SWARM_DIMENSION];
+    array<Vector, SWARM_DIMENSION> particles;
+    array<Vector, SWARM_DIMENSION> particleSpeed;
+    unordered_map<Vector *, Vector> particleBestVector;
+
 
     for (int i = 0; i < SWARM_DIMENSION; i++) {
-        static uniform_real_distribution<double> xyGenerator(0, 10);
-        static uniform_real_distribution<double> zGenerator(0, 4);
+        Vector v(Point::generateRandomPoint(), Point::generateRandomPoint());
+        Vector s(Point::generateRandomSpeed(), Point::generateRandomSpeed());
 
-        swarm[i] = new solution(point(xyGenerator(mt), xyGenerator(mt), zGenerator(mt)),
-                                point(xyGenerator(mt), xyGenerator(mt), zGenerator(mt)),
-                                point(0.01, 0.01, 0.01),
-                                point(0.01, 0.01, 0.01));
+        particles[i] = v;
+        particleBestVector[&particles[i]] = v;
+        particleSpeed[i] = s;
+
+        if (v < bestSolution)
+            bestSolution = v;
     }
 
-    while (iteration < NUMBER_OF_ITERATIONS) {
+    for (int it = 0; it < NUMBER_OF_ITERATIONS; it++) {
         for (int i = 0; i < SWARM_DIMENSION; i++) {
-            solution &s = *swarm[i];
+            double cost = costFunction(particles[i]);
+            if (cost < costFunction(particleBestVector[&particles[i]]))
+                particleBestVector[&particles[i]] = particles[i];
+            if (particles[i] < bestSolution)
+                bestSolution = particles[i];
 
-            double cost = optimizationFunction(s);
-            if (cost < s.bestScore) {
-                s.bestScore = cost;
-                s.bestS1 = s.S1;
-                s.bestS2 = s.S2;
-            }
+            static uniform_real_distribution<double> random01(0.0, 1.0);
 
-            if (cost < g.bestScore) {
-                g.bestScore = cost;
-                g.bestS1 = s.S1;
-                g.bestS2 = s.S2;
-            }
+            Vector &velocity = particleSpeed[i];
+            Vector v1 = w * velocity;
+            Vector v2 = (c1 * random01(mt)) * (particleBestVector[&particles[i]] - particles[i]);
+            Vector v3 = (c2 * random01(mt)) * (bestSolution - particles[i]);
+            particleSpeed[i] = v1 + v2 + v3;
 
-            s.updateSpeed(g);
-            s.updatePosition();
-        }
-
-        iteration++;
-
-        if (iteration % 10000 == 0) {
-            cout << "Progress: " << fixed << setprecision(3) << 100.0 * iteration / NUMBER_OF_ITERATIONS << "%" << endl;
-            cout << "Current minimum: " << fixed << setprecision(3) << g.bestScore << endl;
+            particles[i] = particles[i] + particleSpeed[i];
         }
     }
 
-    cout << "S1" << g.bestS1 << ", " << "S2" << g.bestS2 << endl;
-    cout << "Minimum: " << fixed << setprecision(3) << g.bestScore;
-
-    for (int i = 0; i < SWARM_DIMENSION; i++)
-        delete swarm[i];
-    delete[] swarm;
+    cout << bestSolution << endl;
 
     return 0;
 }
